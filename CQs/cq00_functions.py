5ee29565aa9181f48650d3d2e3e34ff04e1e107f
VulpesNovem <elijahcjives@gmail.com>
2024-08-26 09:35:14 -0400

    8/26/24

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,10 @@
+"""8/26/24 - Functions Challenge Question"""
+
+__author__ = "730745467"
+
+'''Returns a string'''
+def mimic(message: str) -> str:
+    return message
+
+if __name__ == "__main__":
+    print(mimic(message=input("What is your message? ")))
--- lessons/firstprogram.py
+++ lessons/firstprogram.py
@@ -5,6 +5,8 @@ print("Hello World")
 print(1_000_000 + 1_000)
 print("spooky season"[2 + 2])
 print(True)
+
 for attempts in range(3):
+
     print(random())
     print(choice("wxyz"))
