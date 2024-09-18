819e25dfa21c209fb5efa3948ca354dfe2fb6673
VulpesNovem <elijahcjives@gmail.com>
2024-09-18 09:17:02 -0400

    9/18/24

---
+++ CQs/cq03_while_loops.py
@@ -0,0 +1,12 @@
+"""9/18/24 - While Loops Challenge Question"""
+
+__author__ = "730745467"
+
+def num_instances(phrase:str, search_char:str) -> None:
+    index:int = 0
+    count:int = 0
+    while (index < len(phrase)):
+        if (phrase[index] == search_char):
+            count += 1
+        index += 1
+    print(count)
